c8834f851bddca29b236255d5cf0f13c77bda255
Katie Gorbell <katiegorbell@gmail.com>
2022-01-10 10:26:46 -0600

    established routing system for app

---
+++ src/HomePage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './styles.scss'
+
+
+const HomePage = props => {
+    return (
+        <div className="HomePage">
+            
+        </div>
+    )
+}
+
+export default HomePage
