b1b6eea38d78743d0bc6ce40dfcc256f7d3f77ae
Katie Gorbell <katiegorbell@gmail.com>
2022-01-11 00:21:59 -0600

    initialized mastHead component for homepage lander

---
+++ src/HomePage/MastHead/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import image from '../../assets/prepTable.jpeg'
+import Button from '../../components/general/Button'
+import './styles.scss'
+
+
+const MastHead = props => {
+    return (
+        <div className="MastHead" style={{ backgroundImage: `url(${image})` }}>
+            <div className="MastHead__cta">
+                <div className="MastHead__ctaContent">
+                    <span className="MastHead__tagline">Personalize, Build, Organize, and Store your recipes for generations to come.</span>
+                    <div className="MastHead__actionButtons">
+                        <Button size="large">Sign Up</Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MastHead
