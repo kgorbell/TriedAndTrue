7e226902523ff9393c5f72c271446598c5d84050
Katie Gorbell <katiegorbell@gmail.com>
2022-01-11 20:38:27 -0600

    added styling for MastHead cta

--- src/HomePage/MastHead/index.js
+++ src/HomePage/MastHead/index.js
@@ -8,7 +8,9 @@ const MastHead = props => {
     return (
         <div className="MastHead" style={{ backgroundImage: `url(${image})` }}>
             <div className="MastHead__cta">
+                <div className="MastHead__ctaBlur"/>
                 <div className="MastHead__ctaContent">
+                    <span className="MastHead__title">Tried & True</span>
                     <span className="MastHead__tagline">Personalize, Build, Organize, and Store your recipes for generations to come.</span>
                     <div className="MastHead__actionButtons">
                         <Button size="large">Sign Up</Button>
