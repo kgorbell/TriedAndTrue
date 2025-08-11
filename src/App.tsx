224cf2e1fa398c9279f069bd813f2d39458619c1
kaitlyn Gorbell <kaitlyngorbell@kaitlyns-MBP.attlocal.net>
2025-08-10 21:01:29 -0500

    re-initialized app using copilot.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="app-container">
+      <h1>Welcome to TriedAndTrue</h1>
+      <p>This is your Vite + React + TypeScript + Redux Toolkit + SCSS starter app.</p>
+    </div>
+  );
+};
+
+export default App;
