bca2a002fd6cbe0617fe1c415b058853986cad84
Kaitlyn Gorbell <kaitlyngorbell@Kaitlyns-MacBook-Air.local>
2026-03-10 19:35:57 -0500

    Removed deprecated code

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import './App.scss';
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <h1>Welcome to TriedAndTrue</h1>
-      <p>This is your Vite + React + TypeScript + Redux Toolkit + SCSS starter app.</p>
     </div>
   );
 };
