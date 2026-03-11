79ec8466ed87410e4ae5a7ec85074ea49127100e
Kaitlyn Gorbell <kaitlyngorbell@Kaitlyns-MacBook-Air.local>
2026-03-10 19:59:10 -0500

    Removed deprecated code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
+      <Routes>
+        <Route path="/" element={<div />} />
+      </Routes>
     </div>
   );
 };
