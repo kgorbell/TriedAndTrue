2be94f140a4d7391c0cd8d9b9fb24e92dd10b94f
Kaitlyn Gorbell <kaitlyngorbell@Kaitlyns-MacBook-Air.local>
2026-03-11 18:16:45 -0500

    Initialized home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import {
+  Home
+} from './pages';
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
       <Routes>
-        <Route path="/" element={<div />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </div>
   );
